5f716a9965558c60bc9efe2d3fe70c8ff635498e
Tim Work Laptop <tmstani23@gmail.com>
2018-05-09 15:27:44 -0500

    Update 8
    Removed old functions.  Added constructor component App to manage state and render the app.  Added handle remove friends and add friend methods within the App component.  Added setState function.  Whithin the handleRemoveFriend method the set state function takes a current state which is the friends array filtered to include only friends that were not passed into the handleRemoveFriend method.

--- first_app/first-app.jsx
+++ first_app/first-app.jsx
@@ -1,24 +1,60 @@
 
-const name = 'Timothy Stanislav';
-const handle = "@Lutra";
-
-function NameComponent(props) {
-    return <h1>{ props.name }</h1>
-}
-
-function HandleComponent(props) {
-    return <h3>{ props.handle }</h3>
-}
-
-function App() {
+//A friends list function component:
+function FriendsList(props) {
     return (
-        <div id="container">
-            <NameComponent name ='Timothy' />
-            <HandleComponent handle ="@Rukus" />
-        </div>
+        //The friends array is mapped to an anonymous function
+            //And displays a list for each friend in the array:
+        <ul>
+          { props.list.map( (name) => (
+             //Set name as the key for each list item
+             //React needs a unique key to compare states
+             <li  key = { name }>
+                 { name }
+             </li> 
+           )) }  
+        </ul>
     )
 }
+//App component that renders the app to the screen:
+    //Note: Use class component when you want to manage component state
+class App extends React.Component {
+    
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            friends: ["Tom", "Leah", "Isaac"]
+        }
+    }
+
+    handleAddFriend() {
+
+    }
+    //41min in video
+    handleRemoveFriend(name) {
+        //set state as passed in currentState function
+        //Note:the first argument to setState must be a function 
+            //that represents the current state of the component
+        this.setState((currentState) => {
+            return {
+                //set current state friend to 
+                    // a friend that doesn't have the name passed into this component
+                friends: currentState.friends.filter((friend) => friend !== name)
+            }
+        })
+    }
+
+    render() {
+        
+        return (
+            //The friendslist component is passed the friends array:
+            <div>
+                <FriendsList list = { this.state.friends } />
+            </div>
+        )
+    }
+}
 
 
-
+//Renders the app component to the screen.
 ReactDOM.render(<App />, document.getElementById('app'));
